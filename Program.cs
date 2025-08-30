f7e45da34b33943a2724ad7df795192e6c82f106
melinep <meline.pischedda@gmail.com>
2025-08-30 16:04:21 +0200

    Version initiale console StockManager

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace StockManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IInventoryRepository repo = new InMemoryInventoryRepository();
+
+            while (true)
+            {
+                Console.WriteLine("\n=== Gestion de stock — V1 ===");
+                Console.WriteLine("1) Ajouter un produit");
+                Console.WriteLine("2) Mettre à jour quantité");
+                Console.WriteLine("3) Supprimer un produit");
+                Console.WriteLine("4) Lister l'inventaire");
+                Console.WriteLine("0) Quitter");
+                Console.Write("Choix: ");
+                var c = Console.ReadLine();
+            }
+        }
+    }
+}
